24a4320af4285d0a2460273303862198ffc0cf44
Timothy Wilson <timothyjosephwilson@gmail.com>
2016-07-28 14:47:15 -0700

    Not considering x_HI

--- lnL.py
+++ lnL.py
@@ -21,10 +21,10 @@ rho_SFR_error = np.concatenate((np.array(rho_uv_y_error), np.array(rho_ir_y_erro
 rho_uv = Tau.rho(np.array(Tau.uv_z))
 rho_ir = Tau.rho(np.array(Tau.ir_z))
 
-y =  np.concatenate((n['nf0'], tau_adrian, np.array(np.log10(Tau.uv_data)), np.array(np.log10(Tau.ir_data))))
+y =  np.concatenate((tau_adrian, np.array(np.log10(Tau.uv_data)), np.array(np.log10(Tau.ir_data))))
 
-sigma = np.concatenate((x_e_error, tau_adrian_error, rho_SFR_error))
+sigma = np.concatenate((tau_adrian_error, rho_SFR_error))
 
 def m(ap,bp,cp,dp):
 	tau, Q, z, Q_adrian, rho_uv, rho_ir = Tau.calc_tau_Q_rho(ap=ap,bp=bp,cp=cp,dp=dp)
-	return np.concatenate([Q_adrian, [tau[0]], rho_uv, rho_ir])
+	return np.concatenate([[tau[0]], np.log10(rho_uv), np.log10(rho_ir)])
